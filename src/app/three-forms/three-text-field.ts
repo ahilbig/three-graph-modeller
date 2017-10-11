34d2ae6364d5a2557ef31c98c5bbb5b8df6155e8
Andreas Hilbig <ahilbig@me.com>
2017-10-11 08:47:03 +0200

    Credits for inspiration by threejs-input-fields from Fernando Perez added

--- src/app/three-forms/three-text-field.ts
+++ src/app/three-forms/three-text-field.ts
@@ -1,3 +1,16 @@
+/*
+  CREDITS
+  -------
+
+  The "input field" part of my code was inspired by the library threejs-input-fields from Fernando Villar Perez
+  Although Fenando didn't provide a license information credits therefore go to Fernando, you can find his library on github:
+    https://github.com/Vargaf/threejs-input-fields
+
+  Reason for not using threejs-input-fields directly:
+    - Lots of extensions needed
+    - Angular + Typescript wanted
+ */
+
 import * as THREE from 'three';
 import {RenderedObject} from "../graph-renderer/rendered-object";
 
